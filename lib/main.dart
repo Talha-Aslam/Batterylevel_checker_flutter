8b71c63914a8a4bfe3e06aa96cfd4c2ebd7347cd
SubhanOrax <subahnoraxtech.@gmail.com>
2025-08-04 12:43:24 +0500

    Basic _app of battery getter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,11 +10,63 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Method Channel Demo'),
         ),
+        body: const BatteryApp(),
+      ),
+    );
+  }
+}
+
+class BatteryApp extends StatefulWidget {
+  const BatteryApp({super.key});
+
+  @override
+  State<BatteryApp> createState() => _BatteryAppState();
+}
+
+class _BatteryAppState extends State<BatteryApp> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+  String _batteryLevel = 'Unknown battery level.';
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final result = await platform.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    } catch (e) {
+      batteryLevel = "Failed to get battery level: '$e'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            onPressed: _getBatteryLevel,
+            child: const Text('Get Battery Level'),
+          ),
+          Text(_batteryLevel),
+          ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _batteryLevel = 'Unknown battery level.';
+                });
+              },
+              child: const Text("reset battery level")),
+        ],
       ),
     );
   }
--- method_channel_flutter/android/app/src/main/java/com/example/method_channel_flutter/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.method_channel_flutter;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
